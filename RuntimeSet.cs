2617fe93a36d6558ed508fb33f8ba91df52e774f
Radosław Mariowski <rmariowski@gmail.com>
2017-12-22 16:00:03 +0100

    Added RuntimeSet

---
+++ RuntimeSet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ExUnity
+{
+    public abstract class RuntimeSet<T> : ScriptableObject
+    {
+        public List<T> Items = new List<T>();
+
+        /// <summary>
+        /// Adds item to set.
+        /// </summary>
+        /// <param name="item">Item to add</param>
+        public void Add(T item)
+        {
+            if (!Items.Contains(item))
+                Items.Add(item);
+        }
+
+        /// <summary>
+        /// Removes item from set.
+        /// </summary>
+        /// <param name="item">Item to remove</param>
+        public void Remove(T item)
+        {
+            if (Items.Contains(item))
+                Items.Remove(item);
+        }
+    }
+}
